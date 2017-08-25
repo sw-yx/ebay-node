d2e0756dd4ab82a8623edb79f068909e69ad83d2
sw-yx <swyx@swyx.io>
2017-08-25 01:29:30 -0400

    some edits

--- get-items.js
+++ get-items.js
@@ -35,74 +35,51 @@ console.log('Request to the eBay API endpoint for "findCompletedItems" in progre
 // this agg function takes the list of endpoints
 // and iterates through them calling the
 // request operation ( which returns a promise )
-async function buildObjectArray(){
+const buildObjectArray = async () => {
   try {
-    return await getAllResults().then(data=>{
-      let jsonData = [];
-      for (let i of data){
-        jsonData.push(JSON.parse(i).findCompletedItemsResponse[0]['searchResult'][0].item);
-      }
-      jsonData = jsonData.reduce(function(a,b){
-        return a.concat(b);
-      })
-      console.log(`Total unfiltered results: ${jsonData.length}`);
-      return jsonData;
-    })
-        .catch(err=>{console.log(`Promise error: ${err}`)});
-  }
-  catch (err){
+    const data = await getAllResults()
+    let jsonData = data.map(i => JSON.parse(i).findCompletedItemsResponse[0]['searchResult'][0].item)
+                      .reduce((a,b) => a.concat(b))
+    console.log(`Total unfiltered results: ${jsonData.length}`);
+    return jsonData
+  } catch (err) {
     console.log(`Error: ${err}`);
   }
 }
 
-async function getAllResults(){
-  try{
-    let urls = getEndpoints().then();
-    let data = [];
-    let endpoints = await urls; // blocking
-    for (let ep of endpoints){
-      console.log(`Calling endpoint: ${ep}`);
-      data.push(await request(ep).then(response=>{return response}));
-    }
-    return data;
-  }
-  catch (err){
+const getAllResults = async () => {
+  try {
+    let endpoints = await getEndpoints(); // blocking
+    const promiseArray = endpoints.map(request)
+    return await Promise.all(promiseArray)
+  } catch (err){
     console.log(err);
   }
 }
 
 // this is an aggregate function that getsPages and builds the urls (urlBuilder)
 // and then returns the list of endpoints as an array
-async function getEndpoints(){
+const getEndpoints = async () => {
   try {
-    let pages = getPages(url);
-    return await pages.then(count=>{
-      let eta;
-      if ((count*2)/60 < 1){
-        eta = `${Math.round(count*2)} seconds`
-      }
-      else {
-        eta = `${Math.round((count*2)/60)} minutes`
-      }
-      console.log(`Total pages: ${count}\nETA: ${eta}`);
-      return urlBuilder(count);
-    });
-    
-  }
-  catch (err){
+    const count = await getPages(url);
+    const eta = (count*2)/60 < 1 ? 
+      `${Math.round(count*2)} seconds` : 
+      `${Math.round((count*2)/60)} minutes`
+    console.log(`Total pages: ${count}\nETA: ${eta}`);
+    return urlBuilder(count);
+  } catch (err){
     console.log(err);
   }
 }
 
 // this gets and returns the number of pages
-async function getPages(url){
+// async function getPages(url){
+const getPages = async (url) => {
   try {
-      let pageCount = request(url).then(result=>{
-        return JSON.parse(result).findCompletedItemsResponse[0].paginationOutput[0].totalPages[0]
-    })
-    return await pageCount;
-  }
-  catch (err){
+    const result = await request(url)
+    const pageCount = JSON.parse(result).findCompletedItemsResponse[0].paginationOutput[0].totalPages[0]
+    return pageCount;
+  } catch (err){
     console.log(err);
   }
 }
@@ -111,16 +88,9 @@ async function getPages(url){
 // builds and returns an endpoint list
 // as an array
 function urlBuilder(pages){
-  let urls = [];
-  if(pages > 100) {
-    for (let i = 0; i < 100; i++) {
-      urls.push(url + `&paginationInput.pageNumber=${i + 1}`);
-    }
-  }
-  else {
-    for(let i=0; i < pages; i++){
-      urls.push(url + `&paginationInput.pageNumber=${i + 1}`);
-    }
+  let urls = [], maxlength = Math.min(pages, 100);
+  for (let i = 0; i < maxlength; i++) {
+    urls.push(url + `&paginationInput.pageNumber=${i + 1}`);
   }
   return urls;
 }
